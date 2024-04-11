0a67d9bbfba40986011247f393980077f8859d9d
woag <ajyodlowsky@my.waketech.edu>
2024-04-11 08:59:24 -0400

    Made Building Blocks
    credits animations, main menu, currently working on level one

---
+++ Team2-3D/Assets/Scripts/UIController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class UIController : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void OnClickQuit()
+    {
+        Debug.Log("Quit button was clicked");
+        Application.Quit();
+    }
+
+
+    public void OnClickHELP()
+    {
+        SceneManager.LoadScene("HELP");
+    }
+
+
+    public void OnClickCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void OnClickMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void Update()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (currentSceneName == "HELP")
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+
+        if (currentSceneName == "LevelTwo")
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+        if (currentSceneName == "Credits")
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
